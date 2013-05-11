aca44cbccf9eefb378523cd989e0dcdfc7d9c1e9
Francesco Paris <francesco.paris8@gmail.com>
2013-05-11 18:58:52 +0200

    attenzione alla destinazione di larpy

--- 2013-05-10/python/exercise1.py
+++ 2013-05-10/python/exercise1.py
@@ -1,3 +1,7 @@
 #Francesco Paris matricola 453908
 #ho scelto di disegnare una Ferrari F138
 
+
+#in ogni file ho aggiunto la seguente stringa che contiene il percorso nel mio pc dove risiede la cartella larpy
+#per vedere correttamente gli esercizi e' necessario rimuovere tale stringa
+#sys.path.append("/Users/francescoparis/Documents/larpy")
